a92a7420b86f6a2d771e59e9ca6d805d03b8284b
Peter Abraham <79099771+APeterIstvan@users.noreply.github.com>
2023-11-26 15:36:24 +0100

    feat(recipe-list): output a list of recipes with ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://realfood.tesco.com/media/images/Gong-bao-chicken-1400x919-6376346b-85c1-4365-bfe8-54ac7bca9913-0-1400x919.jpg'),
     new Recipe('A test recipe', 'This is simply a test', 'https://realfood.tesco.com/media/images/Gong-bao-chicken-1400x919-6376346b-85c1-4365-bfe8-54ac7bca9913-0-1400x919.jpg')
   ]
 }
