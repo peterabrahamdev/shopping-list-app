5ed7c079f2b01ab83b3ccf8435df7984782e8dc7
Peter Abraham <79099771+APeterIstvan@users.noreply.github.com>
2023-11-26 15:20:32 +0100

    feat(recipe-list): add content to recipes components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://realfood.tesco.com/media/images/Gong-bao-chicken-1400x919-6376346b-85c1-4365-bfe8-54ac7bca9913-0-1400x919.jpg')
+  ]
 }
